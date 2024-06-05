621f7662a0ed08d45d3e0c2c3334a4f12d237ca5
Nikita Dolgolenko <nikitozz29@yandex.ru>
2024-06-05 15:13:22 +0300

    try to maximize window

--- conftest.py
+++ conftest.py
@@ -1,16 +1,25 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
+
+
+chrome_options = Options()
+chrome_options.add_argument("--start-maximized")
+
+firefox_options = Options()
+firefox_options.add_argument("--start-maximized")
 
 
 @pytest.fixture(scope="session")
 def chrome():
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=chrome_options)
     yield driver
     driver.close()
 
 
 @pytest.fixture(scope="session")
 def firefox():
-    driver = webdriver.Firefox()
+    driver = webdriver.Firefox(options=firefox_options)
     yield driver
     driver.close()
